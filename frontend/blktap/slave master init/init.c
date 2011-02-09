c02bdcf163975e3e2d6f6b4cb89e4a11ec4a6ae4
Joao Paulo <jtpaulo@gmail.com>
2011-02-09 10:58:52 +0000

    structure changes

---
+++ frontend/blktap/slave master init/init.c
@@ -0,0 +1,105 @@
+/*
+ * Holeycow-xen
+ * (c) 2010 U. Minho. Written by J. Paulo
+ *
+ * Based on:
+ *
+ * Holeycow-mysql
+ * (c) 2008 José Orlando Pereira, Luís Soares
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "defs.h"
+
+int make_named_socket (const char *filename)
+{
+  struct sockaddr_un name;
+  int sock;
+  size_t size;
+
+  /* Create the socket. */
+  
+  sock = socket (PF_UNIX, SOCK_DGRAM, 0);
+  if (sock < 0)
+    {
+      perror ("socket");
+      exit (EXIT_FAILURE);
+    }
+
+  /* Bind a name to the socket. */
+
+  name.sun_family = AF_FILE;
+  strcpy (name.sun_path, filename);
+
+  /* The size of the address is
+     the offset of the start of the filename,
+     plus its length,
+     plus one for the terminating null byte. */
+  size = (offsetof (struct sockaddr_un, sun_path)
+          + strlen (name.sun_path) + 1);
+
+
+  if (connect (sock, (struct sockaddr *) &name, size) < 0)
+    {
+      perror ("bind");
+      exit (EXIT_FAILURE);
+    }
+
+  return sock;
+}
+
+
+int main(int argc, char* argv[]){
+
+     if (argc!=5) {
+		fprintf(stderr, "usage: master id cowdir stderr_file log_file\n");
+		exit(1);
+     }
+
+     char path[200];
+
+     mkdir(VARPATH,S_IRWXU);
+
+     strcpy(path,VARPATH);
+     strcat(path,argv[1]);
+
+     int newsockfd = make_named_socket (path);
+
+     struct args_sts ms;
+     strcpy(ms.cowdir,argv[2]);
+     strcpy(ms.stderr,argv[3]);
+     strcpy(ms.log,argv[4]);
+
+     send(newsockfd,&ms,sizeof(struct args_sts),0);
+
+
+
+
+ return 0;
+
+}
+
+
