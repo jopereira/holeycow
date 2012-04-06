0bf9fa92ea838ae8ce798016562987c1d806115e
Jose Orlando Pereira <jop@di.uminho.pt>
2012-04-06 16:26:12 +0100

    POSIX aio -> Linux libaio

--- backend/aio/aiobe.c
+++ backend/aio/aiobe.c
@@ -18,11 +18,14 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
+#define _GNU_SOURCE
 #define _LARGEFILE64_SOURCE
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <aio.h>
+#include <fcntl.h>
+#include <libaio.h>
+#include <assert.h>
 
 #include "aiobe.h"
 
@@ -30,66 +33,64 @@
 
 struct aiobe_data {
 	int fd;
+	io_context_t ctx;
+	pthread_t cbt;
 };
 
 struct aiobe_request {
-	struct aiocb64 op;
+	struct iocb iocb;
 	dev_callback_t cb;
 	void* cookie;
 };
 
-static void aiobe_notification(sigval_t val) {
-	int ret;
-	struct aiobe_request* req=(struct aiobe_request*)val.sival_ptr;
-
-	req->cb(req->cookie, aio_return64(&req->op));
+static inline aiobe_complete(struct aiobe_request* req, int ret) {
+	req->cb(req->cookie, ret);
 	free(req);
+}
+
+static void* aiobe_thread(void* p) {
+	struct device* dev = (struct device*)p;
+	struct io_event event;
+	while(1) {
+		int ret=io_getevents(D(dev)->ctx, 1, 1, &event, NULL);
+		if (ret!=1)
+			break;
+		aiobe_complete((struct aiobe_request*) event.data, event.res);
+	}
+	return NULL;
 }
 
 static void aiobe_pwrite(struct device* dev, void* buf, size_t size, off64_t offset, dev_callback_t cb, void* cookie) {
 	int ret;
 	struct aiobe_request* req=(struct aiobe_request*)malloc(sizeof(struct aiobe_request));
+	struct iocb* iocb = &req->iocb;
 	memset(req, 0, sizeof(*req));
 
-	req->op.aio_fildes=D(dev)->fd;
-	req->op.aio_buf=buf;
-	req->op.aio_nbytes=size;
-	req->op.aio_offset=offset;
-
-	req->op.aio_sigevent.sigev_notify = SIGEV_THREAD;
-	req->op.aio_sigevent.sigev_notify_function = aiobe_notification;
-	req->op.aio_sigevent.sigev_notify_attributes = NULL;
-	req->op.aio_sigevent.sigev_value.sival_ptr = req;
-
+	io_prep_pwrite(iocb, D(dev)->fd, buf, size, offset);
 	req->cb=cb;
 	req->cookie=cookie;
+	iocb->data = req;
 
-	ret = aio_write64(&req->op);
-	if (ret < 0)
-		aiobe_notification((sigval_t)(void*)req);
+	ret=io_submit(D(dev)->ctx, 1, &iocb);
+	if (ret!=1)
+		aiobe_complete(req, -1);
 }
 
 static void aiobe_pread(struct device* dev, void* buf, size_t size, off64_t offset, dev_callback_t cb, void* cookie) {
 	int ret;
 	struct aiobe_request* req=(struct aiobe_request*)malloc(sizeof(struct aiobe_request));
+	struct iocb* iocb = &req->iocb;
 	memset(req, 0, sizeof(*req));
 
-	req->op.aio_fildes=D(dev)->fd;
-	req->op.aio_buf=buf;
-	req->op.aio_nbytes=size;
-	req->op.aio_offset=offset;
-
-	req->op.aio_sigevent.sigev_notify = SIGEV_THREAD;
-	req->op.aio_sigevent.sigev_notify_function = aiobe_notification;
-	req->op.aio_sigevent.sigev_notify_attributes = NULL;
-	req->op.aio_sigevent.sigev_value.sival_ptr = req;
-
+	io_prep_pread(iocb, D(dev)->fd, buf, size, offset);
 	req->cb=cb;
 	req->cookie=cookie;
+	iocb->data=req;
 
-	ret = aio_read64(&req->op);
-	if (ret < 0)
-		aiobe_notification((sigval_t)(void*)req);
+	ret=io_submit(D(dev)->ctx, 1, &iocb);
+
+	if (ret!=1)
+		aiobe_complete(req, -1);
 }
 
 static int aiobe_close(struct device* dev) {
@@ -109,11 +110,20 @@ int aiobe_open(struct device* dev, char* path, int flags, mode_t mode) {
 	dev->data = malloc(sizeof(struct aiobe_data));
 
 	int ret = open(path, flags, mode);
-	if (ret>=0) {
-		D(dev)->fd = ret;
-		return 0;
+	if (ret<0) {
+		free(dev->data);
+		return -1;
 	}
 
-	return -1;
+	D(dev)->fd = ret;
+
+	if (io_queue_init(100, &D(dev)->ctx)) {
+		close(D(dev)->fd);
+		free(dev->data);
+		return -1;
+	}
+
+	pthread_create(&D(dev)->cbt, NULL, aiobe_thread, dev);
+	return 0;
 }
 
